ded613103498af09d64ac36a1b134bf128b99f6f
zavacada <jcfcardoso5555@gmail.com>
2025-11-05 23:31:34 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/TabelaFipe/service/ConverteDados.java
@@ -0,0 +1,17 @@
+package br.com.alura.TabelaFipe.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados{
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return objectMapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
